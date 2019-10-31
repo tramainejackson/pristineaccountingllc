5312701d457eebce20d2dc00278c7164cec57837
Tramaine Jackson <tjackson@sicom.com>
2019-10-31 13:29:44 -0400

    First commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="container-fluid vh-100">
+
+        <div class="bg-primary mx-n3 position-fixed w-100 py-3 top">
+            <div class="col-12 text-center">
+                <h2 class="">Coming Soon</h2>
+            </div>
+        </div>
+
+        <div class="row align-items-center flex-column h-100 justify-content-center">
+
+            <div class="text-center animated zoomInUp my-3">
+                <h1 class="text-uppercase">Pristine Accounting LLC</h1>
+            </div>
+
+            <div class="text-center animated zoomInUp delay-1s">
+                <h3 class="">By: Ashley Clark-Jackson</h3>
+            </div>
+        </div>
+
+        <div class="bg-primary mx-n3 position-fixed w-100 py-3 bottom">
+            <div class="col-12 text-center">
+                <h2 class=""><span class="font-weight-bold">Contact For Consultation:</span> <span class="font-italic">adc0426@gmail.com</span></h2>
+            </div>
+        </div>
+    </div>
+@endsection
